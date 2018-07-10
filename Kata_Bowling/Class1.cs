06f42eaeef08aece2c11109352220ec07a475ff3
Dmitry Karabanovich <karabanovich.dmitry@gmail.com>
2018-07-10 12:25:13 +0300

    Add summaries for methods

--- Kata_Bowling/Class1.cs
+++ Kata_Bowling/Class1.cs
@@ -11,12 +11,18 @@ namespace Kata_Bowling
     {
         private readonly int[] _rolls = new int[21];
         private int _currentRoll = 0;
-
+        /// <summary>
+        /// Read score for the roll
+        /// </summary>
+        /// <param name="pins"></param>
         public void Roll(int pins)
         {
             _rolls[_currentRoll++] = pins;
         }
-
+        /// <summary>
+        /// Counting final game score
+        /// </summary>
+        /// <returns></returns>
         public int Score()
         {
             int score = 0;
@@ -42,27 +48,47 @@ namespace Kata_Bowling
 
             return score;
         }
-
+        /// <summary>
+        /// Check Is this a Strike 
+        /// </summary>
+        /// <param name="frameIndex"></param>
+        /// <returns></returns>
         private bool IsStrike(int frameIndex)
         {
             return _rolls[frameIndex] == 10;
         }
-
+        /// <summary>
+        /// Check Is this a Spare
+        /// </summary>
+        /// <param name="frameIndex"></param>
+        /// <returns></returns>
         private bool IsSpare(int frameIndex)
         {
             return _rolls[frameIndex] + _rolls[frameIndex + 1] == 10;
         }
-
+        /// <summary>
+        /// Count Strike bonus
+        /// </summary>
+        /// <param name="frameIndex"></param>
+        /// <returns></returns>
         private int StrikeBonus(int frameIndex)
         {
             return _rolls[frameIndex + 1] + _rolls[frameIndex + 2];
         }
-
+        /// <summary>
+        /// Count Spare bonus
+        /// </summary>
+        /// <param name="frameIndex"></param>
+        /// <returns></returns>
         private int SpareBonus(int frameIndex)
         {
             return _rolls[frameIndex + 2];
         }
-
+        /// <summary>
+        /// Count Sum of pins in one frame
+        /// </summary>
+        /// <param name="frameIndex"></param>
+        /// <returns></returns>
         private int SumOfBallsInFrame(int frameIndex)
         {
             return _rolls[frameIndex] + _rolls[frameIndex + 1];
