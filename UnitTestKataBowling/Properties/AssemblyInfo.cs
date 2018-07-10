d8fa013fb18ebacb22251abc82842ee6f9ea1e77
Dmitry Karabanovich <karabanovich.dmitry@gmail.com>
2018-07-10 12:58:12 +0300

    Create tests for "Game" class

---
+++ UnitTestKataBowling/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestKataBowling")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("UnitTestKataBowling")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ccaa9c55-cee8-487b-9e14-0531221b11b6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestKataBowling/UnitTest1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Kata_Bowling;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestKataBowling
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Game g;
+
+        [TestInitialize]
+        public void InitialiazeGame()
+        {
+            g = new Game();;
+        }
+
+        private void RollMany(int n, int pins)
+        {
+            for (int i = 0; i < n; i++)
+            g.Roll(pins);
+        }
+
+        private void RollSpare()
+        {
+            g.Roll(7);
+            g.Roll(3);
+        }
+
+        [TestMethod]
+        public void TestNullGame()
+        {
+            RollMany(20,0);
+            Assert.AreEqual(0,g.Score());
+        }
+
+        [TestMethod]
+        public void TestExellentGame()
+        {
+            RollMany(12,10);
+            Assert.AreEqual(300,g.Score());
+        }
+
+        [TestMethod]
+        public void TestOneSpare()
+        {
+            RollSpare();
+            g.Roll(6);
+            RollMany(17,0);
+            Assert.AreEqual(22,g.Score());
+        }
+
+        [TestMethod]
+        public void TestOneStrike()
+        {
+            g.Roll(10);
+            g.Roll(5);
+            g.Roll(3);
+            RollMany(16,0);
+            Assert.AreEqual(26,g.Score());
+        }
+
+        [TestMethod]
+        public void TestAllThree()
+        {
+            RollMany(20,3);
+            Assert.AreEqual(60,g.Score());
+        }
+
+        [TestMethod]
+        public void TestAllSpare()
+        {
+            RollMany(21,5);
+            Assert.AreEqual(150,g.Score());
+        }
+    }
+}
